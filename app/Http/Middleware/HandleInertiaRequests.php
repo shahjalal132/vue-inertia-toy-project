c62279fadada691f0bdf95cd6961118e41d2bdaa
Shah Jalal <rjshahjalal132@gmail.com>
2025-08-18 16:43:27 +0600

    remove docs links from dashboard and provide dynamic username to layout component

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -45,7 +45,7 @@ class HandleInertiaRequests extends Middleware
             'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
                 'user' => $request->user(),
-                'username' => "Jalal"
+                'username' => $request->user() ? $request->user()->name : null,
             ],
             'ziggy' => [
                 ...(new Ziggy)->toArray(),
